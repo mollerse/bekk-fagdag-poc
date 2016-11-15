09eae2228bf5054546e923acdf9ec98796282ab9
Stian Veum Møllersen <stian.moellersen@gmail.com>
2016-11-15 21:04:16 +0100

    Kindasortaworking

--- client.js
+++ client.js
@@ -2,21 +2,112 @@ var io = require('socket.io-client');
 
 var socket = io('http://localhost:3000');
 
+var dots = {};
+var me = {};
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var HEIGHT = canvas.height;
+var WIDTH = canvas.width;
+
+var WINDOW_W = window.innerWidth;
+var WINDOW_H = window.innerHeight;
+
+var DX = random2(-1, 1);
+var DY = random2(-1, 1);
+
 socket.on('connect', function(){
   socket.emit('join');
 });
 
 socket.on('move', function(data){
-  console.log(data);
+  dots[data.tag] = data;
 });
 
 socket.on('join', function(data){
-  console.log(data);
-  console.log('welcome to the game');
+  dots[data.tag] = data;
+  me = data;
 });
 
-socket.on('disconnect', function(){
-  console.log('you disconnected');
+socket.on('disconnect', function(data){
+  var dot = dots[data.tag];
+  if(dot) {
+    dot.moving = false;
+  }
 });
 
-socket.emit('move', {tag: Math.random(), x: 10, y: 10});
+function random2(l, u) {
+  return l + Math.random()*(u-l);
+}
+
+function drawDot(dot, i, dots) {
+  ctx.fillStyle = 'white';
+  ctx.strokeStyle = 'teal';
+
+  Object.keys(dots).forEach(function(tag) {
+    if(tag === me.tag) { return; }
+
+    var d = dots[tag];
+
+    if(Math.sqrt(Math.pow(d.x-dot.x, 2) + Math.pow(d.y-dot.y, 2)) < 100) {
+      ctx.beginPath();
+      ctx.moveTo(dot.x, dot.y);
+      ctx.lineTo(d.x, d.y);
+      ctx.closePath();
+      ctx.stroke();
+    }
+  });
+
+  var r = 2.5;
+  if(dot === me) {
+    ctx.fillStyle = 'red';
+    r = 5;
+  }
+
+  ctx.beginPath();
+  ctx.moveTo(dot.x, dot.y);
+  ctx.arc(dot.x, dot.y, r, 0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function draw() {
+  var lastFrame = 0;
+
+  function inner(t) {
+    requestAnimationFrame(inner);
+
+    var delta = t - lastFrame;
+
+    if(lastFrame && delta < (16/(25/60))+1) { return; }
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    Object.entries(dots)
+      .filter(function([tag, dot]) {
+        return dot.x < (me.x + 0.5 * WINDOW_W)
+        && dot.x > (me.x - 0.5 * WINDOW_W)
+        && dot.y < (me.y + 0.5 * WINDOW_H)
+        && dot.y > (me.y - 0.5 * WINDOW_H);
+      })
+      .map(([tag,dot]) => dot)
+      .forEach(drawDot);
+
+    var _x = me.x - 0.5*WINDOW_W ;
+    var _y = me.y - 0.5*WINDOW_H;
+
+    canvas.style.transform = `translate(${-_x}px, ${-_y}px)`;
+
+    me.x = me.x + DX;
+    me.y = me.y + DY;
+    socket.emit('move', me);
+  }
+
+  requestAnimationFrame(inner);
+}
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+draw();
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 
+var dots = [];
+
+var CANVAS_X = 3000;
+var CANVAS_Y = 3000;
+
 app.get('/', function(req, res){
   fs.createReadStream('index.html').pipe(res);
 });
@@ -12,16 +17,33 @@ app.get('/bundle.js', function(req, res){
 
 http.listen(3000);
 
+function random2(l, u) {
+  return l + Math.random()*(u-l);
+}
+
+for (var i = 0; i < 500; i++) {
+  dots.push({tag: i, x: random2(0, CANVAS_X), y: random2(0, CANVAS_Y)});
+}
+
 io.on('connection', function(socket) {
+  var tag = dots.length;
+
   socket.on('join', function(msg){
-    socket.emit('join', {tag: '#1', x: Math.random(), y: Math.random()});
+    var me = {tag, x: random2(0, CANVAS_X), y: random2(0, CANVAS_Y)};
+    socket.emit('join', me);
+
+    dots.forEach(function(dot) {
+      socket.emit('move', dot);
+    });
+
+    dots.push(me);
   });
 
   socket.on('move', function(msg) {
-    socket.broadcast.emit('move', {tag: msg.tag, x: msg.x, y: msg.y});
+    socket.broadcast.emit('move', {tag, x: msg.x, y: msg.y});
   })
 
   socket.on('disconnect', function() {
-    io.emit('disconnect', {tag: '#1'});
-  })
+    io.emit('disconnect', {tag});
+  });
 })
